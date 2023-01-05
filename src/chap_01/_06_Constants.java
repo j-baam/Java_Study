5718c7bf5dab8f1c44ace17726b924313ef941b1
이준범 <97700761+j-baam@users.noreply.github.com>
2023-01-05 12:14:26 +0900

    study

---
+++ src/chap_01/_06_Constants.java
@@ -0,0 +1,10 @@
+//상수 <- 한번 적으면 변할수 없음
+
+package chap_01;
+
+public class _06_ddd {
+        public static void main(String[] args) {
+                System.out.println("안녕하세요1222223");
+        }
+}
+                     System
