b772c3cc595fbb0725369a8f2e63c59a2842781f
이준범 <97700761+j-baam@users.noreply.github.com>
2023-01-05 13:02:45 +0900

    study

--- src/chap_01/_06_Constants.java
+++ src/chap_01/_06_Constants.java
@@ -2,9 +2,15 @@
 
 package chap_01;
 
-public class _06_ddd {
-        public static void main(String[] args) {
-                System.out.println("안녕하세요1222223");
-        }
+public class _06_Constants {
+        public static void main(String[] args){
+                final String KR_COUNTRY_CODE + "+82"; // final <- 상수
+                KR_COUNTRY_CODE = "+8282";
+                System.out.println(KR_COUNTRY_CODE);
+
+                final double PI = 3.141592;
+                final String DATE_OF_BRITH = "2001-06-04"; // 상수를 만들때 대문자        
+                }
+       
 }
-                     System
+                     
---
+++ src/chap_01/_07_TypeCasting.java
@@ -0,0 +1,20 @@
+// 형 변환
+
+package chap_01;
+
+public class _07_TypeCasting {
+    public static void main(String[] args) {
+        // 형 변환 TypeCasting
+        // 정수형에서 실수형
+        // 실수형에서 정수형
+
+        //int score = 93 + 93.5; //결과가 double(실수)형태인데  int(정수)형으로 받아 오류 
+
+    int score = 93;
+    System.out.println(score);  //93
+    System.out.println((float)score); //형 변환을 위해 앞에 ()을 넣어 변환
+
+    }
+
+}
+
