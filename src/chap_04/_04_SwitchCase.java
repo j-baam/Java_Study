ad658cccef499ec5f0e1d440b28e3e517ff3c2bd
Beom <wnsqja4317@gmail.com>
2023-01-10 23:19:10 +0900

    2023.1.10 Tuesday

---
+++ src/chap_04/_04_SwitchCase.java
@@ -0,0 +1,19 @@
+package chap_04;
+
+public class _04_SwitchCase {
+    public static void main(String[] args) {
+        // Switch Case
+
+        // 석차에 따른 장학금 지급
+        // 1등 : 전액장학금
+        // 2등 : 반액장학금
+        // 3등 : 반액장학금
+        // 그 외  : 장학금 대상 아님
+
+        int ranking = 1; // 1등
+        if (ranking == 1) {
+            System.out.println("전액 장학금");
+        }
+
+    }
+}
