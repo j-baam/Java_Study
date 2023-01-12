7924b6e1e20caaed106677a1da8bc3c02d53039a
Beom <wnsqja4317@gmail.com>
2023-01-12 23:00:28 +0900

    Day 11

--- src/chap_04/_04_SwitchCase.java
+++ src/chap_04/_04_SwitchCase.java
@@ -10,10 +10,41 @@ public class _04_SwitchCase {
         // 3등 : 반액장학금
         // 그 외  : 장학금 대상 아님
 
-        int ranking = 1; // 1등
+       /* int ranking = 1; // 1등
         if (ranking == 1) {
             System.out.println("전액 장학금" );
-        }
+        }*/
 
+        // Switch Case 문을 이용
+       int ranking = 3;
+        switch ( ranking ) {
+            case 1:
+                System.out.println("전액 장학금");
+                break; // case1 만족하지 않으면 빠져나오기
+            case 2:
+                System.out.println("반액 장학금");
+                break;
+            case 3:
+                System.out.println("반액 장학금");
+                break;
+            default:
+                System.out.println("장학금 대상 아님");
+        }
+        System.out.println("조회완료");
+        // case 2와 3을 통합
+        ranking = 2;
+        switch ( ranking ) {
+            case 1:
+                System.out.println("전액 장학금");
+                break; // case1 만족하지 않으면 빠져나오기
+            case 2:
+            case 3:
+                System.out.println("반액 장학금");
+                break;
+            default:
+                System.out.println("장학금 대상 아님");
+        }
+        System.out.println("조회완료");
     }
+
 }
