3f25832225fa16f9b248e509379a35c54dfd6c7b
Beom <wnsqja4317@gmail.com>
2023-01-14 17:51:10 +0900

    Day 13

---
+++ src/chap_04/_09_MultipleTable.java
@@ -0,0 +1,29 @@
+package chap_04;
+
+public class _09_MultipleTable {
+    public static void main(String[] args) {
+        // 구구단
+        // 2단
+
+
+        for (int i = 0; i <= 10 ; i++) {
+            int number = 2;
+            int sum = number * i;
+            System.out.println(number + "*" + i +" 의 값은 = "+ sum);
+
+        }
+        for (int i = 0; i <= 10 ; i++) {
+            int number = 6;
+            int sum = number * i;
+            System.out.println(number + "*" + i +" 의 값은 = "+ sum);
+
+        }
+        for (int i = 2; i <= 10; i++) {
+            for (int j = 1; j <= 10; j++) {
+                System.out.println(i + "x" + j + " = "+ (i*j));
+
+            }
+
+        }
+    }
+}
