637acfe7e2f91b9e7b1915d6f3d4101d412057c9
Beom <wnsqja4317@gmail.com>
2023-01-15 19:19:01 +0900

    Day 14

--- src/chap_04/_09_MultipleTable.java
+++ src/chap_04/_09_MultipleTable.java
@@ -20,7 +20,7 @@ public class _09_MultipleTable {
         }
         for (int i = 2; i <= 10; i++) {
             for (int j = 1; j <= 10; j++) {
-                System.out.println(i + "x" + j + " = "+ (i*j));
+                System.out.println(i + "x" + j +   " = "+ (i*j));
 
             }
 
