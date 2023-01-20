a0d6b13b95566d1a3dafc43e70e41a5b7556b549
Beom <wnsqja4317@gmail.com>
2023-01-20 22:13:06 +0900

    Day 19

--- src/chap_05/_01_Array.java
+++ src/chap_05/_01_Array.java
@@ -3,6 +3,26 @@ package chap_05;
 public class _01_Array {
     public static void main(String[] args) {
         // 배열 : 같은 자료형의 값 여러 개를 저장하는 연속된 공간.
+        String coffeeA = "아메리카노";
+        String coffeeB = "카페모카";
+        String coffeeC = "라떼";
+        String coffeeD = "카푸치노";
+
+        System.out.println(coffeeA + " 하나");
+        System.out.println(coffeeB + " 하나");
+        System.out.println(coffeeC + " 하나");
+        System.out.println(coffeeD + " 하나");
+        System.out.println("주세요 ");
+
+        // 배열 선언 첫 번째 방법
+        String[] coffees = new String[4];
+
+        // 두 번째 방법
+        /* String coffees[] = new String[4]; */
+        coffees[0] = "아메리카노"; // 0 부터 시작
+        coffees[1] = "카페모카";
+        coffees[2] = "라떼";
+        coffees[3] = "카푸치노";
 
 
     }
