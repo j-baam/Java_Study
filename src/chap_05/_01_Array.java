48189330b008003d9d1f0130fc5e86213149d393
Beom <wnsqja4317@gmail.com>
2023-01-22 20:48:45 +0900

    Day 20

--- src/chap_05/_01_Array.java
+++ src/chap_05/_01_Array.java
@@ -15,15 +15,38 @@ public class _01_Array {
         System.out.println("주세요 ");
 
         // 배열 선언 첫 번째 방법
-        String[] coffees = new String[4];
+        //String[] coffees = new String[4];
 
         // 두 번째 방법
         /* String coffees[] = new String[4]; */
-        coffees[0] = "아메리카노"; // 0 부터 시작
+       /* coffees[0] = "아메리카노"; // 0 부터 시작
         coffees[1] = "카페모카";
         coffees[2] = "라떼";
-        coffees[3] = "카푸치노";
+        coffees[3] = "카푸치노";*/
 
 
-    }
+        // 세 번째 방법
+        //String[] coffees = new String[] {"아메리카노", "카페모카", "라떼", "카푸치노"};
+
+        // 네 번째 방법
+        String[] coffees = {"아메리카노", "카페모카", "라떼", "카푸치노"};
+
+        System.out.println("-----------------------");
+        // 커피주문
+        System.out.println(coffees[0] + " 하나");
+        System.out.println(coffees[1] + " 하나");
+        coffees[2] = "에스프레소";
+        System.out.println(coffees[2] + " 하나");
+        System.out.println(coffees[3] + " 하나");
+        System.out.println("주세요.");
+
+
+    // 다른 자료형?
+    int[] i = new int[3];
+    i[0]=1;
+    i[1]=2;
+    i[2]=3;
+    double[] d = new double[] {10.0, 11.2, 13.5};
+    boolean[] b = {true, true, false};
+}
 }
---
+++ src/chap_05/_02_ArrayLoop.java
@@ -0,0 +1,4 @@
+package chap_05;
+
+public class _02_ArrayLoop {
+}
