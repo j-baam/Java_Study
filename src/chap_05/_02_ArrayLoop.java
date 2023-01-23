1e831f57db9f6730849c5eefe230609cb31720c0
Beom <wnsqja4317@gmail.com>
2023-01-23 22:47:55 +0900

    Day 21

--- src/chap_05/_02_ArrayLoop.java
+++ src/chap_05/_02_ArrayLoop.java
@@ -1,4 +1,26 @@
 package chap_05;
 
 public class _02_ArrayLoop {
+    public static void main(String[] args) {
+        // 배열의 순회
+        String[] coffees = {"아메리카노", "카페모카", "라떼", "카푸치노"};
+
+        for (int i = 0; i < 4; i++) {
+            System.out.println(coffees[i]+ "하나");
+
+        }
+        System.out.println("주세요");
+
+        System.out.println("++++++++++++++++");
+
+        // 배열의 길이를 이용한 순회
+        for (int i = 0; i < coffees.length; i++) {       //coffees.length <- 배열의 크기
+            System.out.println(coffees[i]+ "하나");
+
+        }
+        System.out.println("주세요");
+
+        // enhanced for (for-each) 반복문
+        for (String coffee : )
+    }
 }
