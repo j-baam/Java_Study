66cfcc3cc806584e11802443f6398227c6672703
Beom <wnsqja4317@gmail.com>
2023-01-25 19:17:02 +0900

    Day 23

--- src/chap_05/_03_MultiArray.java
+++ src/chap_05/_03_MultiArray.java
@@ -3,5 +3,23 @@ package chap_05;
 public class _03_MultiArray {
     public static void main(String[] args) {
         // 다차원 배열 (2차원 배열)
+
+        // 소규모 영화관 좌석
+        // A1 - A5
+        // B1 - B5
+        // C1 - C5
+
+        String[] seatA = {"A1", "A2", "A3", "A4", "A5"};
+        String[] seatB = {"B1", "B2", "B3", "B4", "B5"};
+        String[] seatC = {"C1", "C2", "C3", "C4", "C5"};
+
+        // 3 x 5 크기의 2차원 배열
+        String[][] seats = new String[][] { // 차원이 늘어날때마다 대괄호를 하나씩
+                {"A1", "A2", "A3", "A4", "A5"},
+                {"B1", "B2", "B3", "B4", "B5"},
+                {"C1", "C2", "C3", "C4", "C5"}
+
+        };
+        System.out.println(seats [2][3]);
     }
 }
