35e2fc5347e4de0255438699a2322f7ce16a6d73
Beom <wnsqja4317@gmail.com>
2023-02-03 03:34:09 +0900

    Day 27

--- src/chap_05/_04_MultiArrayLoop.java
+++ src/chap_05/_04_MultiArrayLoop.java
@@ -15,5 +15,21 @@ public class _04_MultiArrayLoop {
             }
             System.out.println();
         }
+
+        System.out.println("---------------------");
+
+        String[][] seats2 = {
+                {"A1", "A2", "A3"},
+                {"B1", "B2", "B3", "B4"},
+                {"C1", "C2", "C3", "C4", "C5"}
+        };
+
+
+        for (int i = 0; i < seats2.length; i++) {  // seats2.length;  <- 배열의 길이
+            for (int j = 0; j < seats2[i].length; j++) {         // 5칸
+                System.out.print(seats2[i][j] + " "); // A1 A2 A3
+            }
+            System.out.println();
+        }
     }
 }
