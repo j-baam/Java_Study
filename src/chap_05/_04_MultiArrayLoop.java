ce8305130ad3f91cbea23ef06409344b5886cfcb
Beom <wnsqja4317@gmail.com>
2023-02-04 23:05:49 +0900

    Day 28

--- src/chap_05/_04_MultiArrayLoop.java
+++ src/chap_05/_04_MultiArrayLoop.java
@@ -31,5 +31,27 @@ public class _04_MultiArrayLoop {
             }
             System.out.println();
         }
+
+        System.out.println("------------------------");
+        // 세로 크기 10 x 가로 크기 15에 해당하는 영화관 좌석
+        String[][] seats3 = new String[10][15];
+        String[] eng = {"A", "B", "C", "D", "E","F","G","H","I","J"};
+        for (int i = 0; i < seats3.length; i++) {
+            for (int j = 0; j < seats3[i].length; j++) {
+                seats3[i][j] = eng[i] + (j + 1);          //eng[i] <- 영어 j+1 <- 숫자
+
+            }
+
+        }
+        // 표 구매
+        seats3[7][8] = "매진";
+        seats3[7][9] = "매진";
+        // 좌석번호 확인
+        for (int i = 0; i < seats3.length; i++) {  // seats2.length;  <- 배열의 길이
+            for (int j = 0; j < seats3[i].length; j++) {         // 5칸
+                System.out.print(seats3[i][j] + " "); // A1 A2 A3
+            }
+            System.out.println();
+        }
     }
 }
