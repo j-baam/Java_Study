94f670f18711b6528b8bf262aa6256b165a450d3
Beom <wnsqja4317@gmail.com>
2023-02-05 15:07:33 +0900

    Day 29

---
+++ src/chap_05/_05_ASCII.java
@@ -0,0 +1,14 @@
+package chap_05;
+
+public class _05_ASCII {
+    public static void main(String[] args) {
+        // 아스키 코드 (ANSI) : 미국 표준 코드
+        char c = 'A';
+        System.out.println(c);
+        System.out.println((int)c);
+
+        int d = 65;
+        System.out.println((char)c);
+
+    }
+}
